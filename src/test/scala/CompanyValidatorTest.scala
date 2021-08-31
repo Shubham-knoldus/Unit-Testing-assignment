c2556c5655c2587f00e10b065830d6c36fd154b6
Shubham-knoldus <shubham.shrivastava@knoldus.com>
2021-08-31 19:51:16 +0530

    Unit Integration testing assignment

---
+++ src/test/scala/CompanyValidatorTest.scala
@@ -0,0 +1,12 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import com.knoldus.models.Company
+import com.knoldus.validator.CompanyValidator
+
+class CompanyValidatorTest extends AnyFlatSpec {
+  "Company Name" should "exits" in {
+    val companyObject = new CompanyValidator()
+    val company = Company("Knoldus", "abc@gmail.com", "noida")
+    assert(companyObject.companyIsValid(company))
+  }
+}
+
